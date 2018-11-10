6d251e55ac6d073c0997087ccd102c530f85e210
cypconnet <cyprien.connet@capgemini.com>
2018-11-10 11:25:20 +0100

    new test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/wordCloudGenerator'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/wordCloudGenerator/index.html'));
+res.sendFile(path.join(__dirname,'/dist/wordCloudRetro2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
