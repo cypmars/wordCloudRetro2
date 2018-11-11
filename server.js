cc04aacbf7be2d2804fa5bce7799012d1bbcf1cb
cypconnet <cyprien.connet@capgemini.com>
2018-11-11 12:05:41 +0100

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/wordCloudGenerator'));
+app.use(express.static('./dist/wordCloudRetro2'));
 
 app.get('/*', function(req,res) {
     
